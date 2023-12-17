d9fb5dba5500c606df55d96e0657c715d78eaf51
Pedro Campos <pedro@cdias.dev>
2023-12-16 23:03:26 -0300

    fix: sdk intialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useCallback, useEffect } from 'react';
+
 import { Home } from 'routes/index';
 
+import { sdkStyles } from 'constants/sdkStyles';
 import { PaymentProvider } from 'context/paymentContext';
 
-export const App = () => (
-  <PaymentProvider>
-    <Home />
-  </PaymentProvider>
-);
+export const App = () => {
+  const setupSDK = useCallback(async () => {
+    await window.paytheory.payTheoryFields({
+      apiKey: process.env.REACT_APP_PAYTHEORY_API_KEY,
+      styles: sdkStyles,
+    });
+  }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setupSDK();
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, [setupSDK]);
+
+  return (
+    <PaymentProvider>
+      <Home />
+    </PaymentProvider>
+  );
+};
